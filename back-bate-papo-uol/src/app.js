3fc594442f5ad2f3e627fdc44ba1749d92a64650
Igor Holanda <oigorholanda@gmail.com>
2023-01-13 22:32:25 -0300

    fix: post participants

--- back-bate-papo-uol/src/app.js
+++ back-bate-papo-uol/src/app.js
@@ -1,59 +1,64 @@
-import express from "express"
-import cors from "cors"
-import dotenv from "dotenv"
-import { MongoClient, ObjectId } from "mongodb"
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import { MongoClient, ObjectId } from "mongodb";
 
+const app = express();
+app.use(cors());
+app.use(express.json());
+dotenv.config();
 
-const app = express()
-app.use(cors())
-app.use(express.json())
-dotenv.config()
-
-const mongoClient = new MongoClient(process.env.DATABASE_URL)
+const mongoClient = new MongoClient(process.env.DATABASE_URL);
 let db;
 
-
-    mongoClient.connect().then( () => {
-        db = mongoClient.db()
-        console.log("Conectado ao banco de dados");
-    }) .catch ( (error) => {
-        console.log("Algo na conexão com o banco deu errado", error);
-    })
-    
-    
+mongoClient
+  .connect()
+  .then(() => {
+    db = mongoClient.db();
+    console.log("Conectado ao banco de dados");
+  })
+  .catch((error) => {
+    console.log("Algo na conexão com o banco deu errado", error);
+  });
 
 app.get("/participants", async (req, res) => {
-    const users = await db.collection("participants").find().toArray()
-    res.send(users)
-})
+  const users = await db.collection("participants").find().toArray();
+  res.send(users);
+});
 
 app.post("/participants", async (req, res) => {
-    const username = req.body.name
+  const name = req.body.name;
 
-    if (!username) {
-       return res.status(422).send("Nome de usuário inválido")
-    }
-    await db.collection("participants").insertOne({ username })
-    res.status(201).send("Usuário Cadastrado")
-})
+  if (!name) return res.status(422).send("Nome de usuário inválido");
+
+  try {
+    const user = await db.collection("participants").findOne({ name });
+
+    if (user) return res.status(409).send("Nome de usuário já cadastrado");
+
+    await db.collection("participants").insertOne({ 
+        name: name,
+        lastStatus: Date.now()
+     });
+    return res.status(201).send("Usuário Cadastrado");
+
+  } catch (error) {
+    console.log("Erro ao inserir o usuário", error);
+  }
+});
 
 app.delete("/participants/:id", async (req, res) => {
-    const { id } = req.params
-   
-    try {
-       await db.collection("participants").deleteOne( {_id: ObjectId(id)})
+  const { id } = req.params;
 
-        res.status(202).send("OK")
+  try {
+    await db.collection("participants").deleteOne({ _id: ObjectId(id) });
 
-    } catch (error) {
-        console.log("Erro ao deletar o post", error); 
-    }
-    
+    res.status(202).send("OK");
+  } catch (error) {
+    console.log("Erro ao deletar o post", error);
+  }
+});
 
-})
+const PORT = process.env.PORT;
 
-
-
-const PORT = process.env.PORT
-
-app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`))
+app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
